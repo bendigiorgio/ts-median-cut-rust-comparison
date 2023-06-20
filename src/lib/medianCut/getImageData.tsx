9297f626da77297d0cf9e751b1ec7ea9459ff2f5
bendigiorgio <bendigiorgio@gmail.com>
2023-06-20 12:49:56 +0900

    broken

--- src/lib/medianCut/getImageData.tsx
+++ src/lib/medianCut/getImageData.tsx
@@ -1,18 +1,26 @@
 import { StaticImageData } from "next/image";
 
-export const extractData = (imageUrl: string | StaticImageData) => {
+export const extractData = async (imageUrl: string | StaticImageData) => {
+  let loadingFinished = false;
   const canvas = document.createElement("canvas");
   const ctx = canvas.getContext("2d");
   const img = new Image();
 
   if (typeof imageUrl === "object") imageUrl = imageUrl.src;
   img.src = imageUrl;
+  img.crossOrigin = "Anonymous";
 
   if (!ctx) throw new Error("Could not get context");
-  img.onload = () => {
-    canvas.width = img.width;
-    canvas.height = img.height;
-    ctx.drawImage(img, 0, 0);
+  const loadImage = async () => {
+    img.onload = () => {
+      canvas.width = img.width;
+      canvas.height = img.height;
+      ctx.drawImage(img, 0, 0);
+      loadingFinished = true;
+    };
   };
-  return ctx.getImageData(0, 0, canvas.width, canvas.height);
+  const imageData = await loadImage().then(() => {
+    return ctx.getImageData(0, 0, canvas.width, canvas.height);
+  });
+  return imageData;
 };
